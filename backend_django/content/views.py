fe369b011b33710b65204ebeaae2571656f112a3
luisdionicio <luis.dionicio@tecsup.edu.pe>
2026-05-13 21:22:02 -0500

    skillS

--- backend_django/content/views.py
+++ backend_django/content/views.py
@@ -1,3 +1,4 @@
+# pyrefly: ignore [missing-import]
 from rest_framework import viewsets, permissions
 from .models import Guide, Hospital, News
 from .serializers import GuideSerializer, HospitalSerializer, NewsSerializer
